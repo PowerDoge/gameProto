ac491c2ba000d7c6318a59ccb9211545cd2e7959
PowerDoge <64737480+PowerDoge@users.noreply.github.com>
2020-11-09 21:15:47 +0300

    Add files via upload

--- resetButton.js
+++ resetButton.js
@@ -1,18 +1,14 @@
-class ResetButton {
-  constructor(x, y) {
-    this.x = x;
-    this.y = windowHeight - floor((windowHeight - board.w)/4);
-    this.a = 1.5 * a;
-    this.b = floor(a * 0.4);
-
-  }
-
+function ResetButton(x, y) {
+  this.x = x;
+  this.y = windowHeight - floor((windowHeight - board.w) / 4);
+  this.a = 1.5 * a;
+  this.b = floor(a * 0.4);
 
   show = function() {
     fill(255);
     rect(this.x - this.a / 2, this.y - this.b / 2, this.a, this.b)
     textAlign(CENTER);
-    textSize(this.b-3);
+    textSize(this.b - 3);
     stroke(0);
     fill(0);
     text('RESET', this.x, this.y + this.b / 2 - 3);
